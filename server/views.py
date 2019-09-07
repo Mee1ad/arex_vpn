fa599dadd4e7f3bb62aa035891fce4b1527d8f5c
meelad <soheilravasani@gmail.com>
2019-09-08 00:26:13 +0430

    test

--- server/views.py
+++ server/views.py
@@ -17,9 +17,13 @@ class Login(View):
         # device_id = data['device_id']
         try:
             voucher = Vouchers.objects.select_related('profile').filter(password=code + 1).first()
+            voucher.profile.name = voucher.profile.name.lower()
             print(voucher.profile.name)
-            regex = re.search(r'(.+)mb-(\d)m', voucher.profile.name)
+            regex = re.search(r'(\d+)mb-(\d+)m', voucher.profile.name)
             max_data = int(regex[1]) * 1000000  # MegaByte to Byte
+            if regex is None:
+                regex = re.search(r'(\d+)g-(\d+)m', voucher.profile.name)
+                max_data = int(regex[1]) * 1000000000  # GigaByte to Byte
             max_time = int(regex[2]) * 30 * 24 * 60 * 60  # Month to Second
             data = max_data - voucher.data_used
             time = max_time - voucher.time_used
