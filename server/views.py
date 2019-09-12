eb3c9dc06ee0ffd25385f17209cae1e2b70dfd18
meelad <soheilravasani@gmail.com>
2019-09-12 21:15:18 +0430

    fix

--- server/views.py
+++ server/views.py
@@ -29,8 +29,8 @@ class Login(View):
             time_usage_percent = int(time_remaining / voucher.time_cap * 100)
         except Exception:
             return JsonResponse({'message': 'ok'}, status=401)
-        res = {'total_data': total_data, 'total_time': total_time, 'data_used': data_used, 'time_used': time_used,
-               'data_remaining': data_remaining, 'time_remaining': time_remaining,
+        res = {'total_data': total_data, 'total_time': 84600, 'data_used': data_used, 'time_used': 3600,
+               'data_remaining': data_remaining, 'time_remaining': 60,
                'data_usage_percent': data_usage_percent, 'time_usage_percent': time_usage_percent}
         return JsonResponse(res)
 
